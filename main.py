13f1e6e1c8aead2291d2897d25bfe64e367c42fe
Mikhail Byrdukovsky <burmspb@gmail.com>
2026-04-12 19:51:42 +0300

    final revision of the code

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 """Точка входа"""
 
-from src.base import SkyMapCoordinator
+from src.aircraft_collector import user_interaction
 
 if __name__ == "__main__":
-
-    country_name = input("Введите название страны: ")
-    fly_obj = SkyMapCoordinator()
-    #print(fly_obj.extraction_countries_list)
-    country_code = fly_obj.extraction_countries_list[country_name]
-    print(f"Код для страны {country_name}: {country_code}")
-    print(fly_obj.extraction_border_country(f"Рамка для страны: {country_name}"))
-    # print(f"Данные о самолетах над страной {country_name}")
-    # print(fly_obj.extraction_aircraft_info())
+    user_interaction()
